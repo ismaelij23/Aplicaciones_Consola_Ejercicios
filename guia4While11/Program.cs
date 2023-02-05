a7eefbb7fa1997d2c6d556ab861dc2d267df60f7
ismaelij23 <ismaeljuaristi23@gmail.com>
2023-02-05 00:17:03 -0300

    Agregue comentarios

--- guia4While11/Program.cs
+++ guia4While11/Program.cs
@@ -46,15 +46,6 @@ namespace guia4While11
                     Console.WriteLine("Ingrese otro número, si desea para ejecución ingrese 0");
                     numero = int.Parse(Console.ReadLine());
                 }
-                // 3 7 13 59 primos y positivos 4
-                // 4 6 8 10 pares y positivos 4
-                // 22 26 pos 2 
-                // -1 -5 -7 
-
-                //pos = 10
-                //neg = 3
-                //pares = 6
-                //primos = 4
 
                 Console.WriteLine("\n\nRESULTADOS");
                 Console.WriteLine("Positivos: " + contPos + "\nNegativos: " + contNeg + "\nPares: " + contPares + "\nPrimos: " + contPrimos + "\n");
