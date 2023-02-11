8dddfec303142f0b9c9c8c65f1f09bd362e9ecfb
ismaelij23 <ismaeljuaristi23@gmail.com>
2023-02-10 21:15:26 -0300

    + ejercicios terminados

---
+++ guia8_5/Program.cs
@@ -0,0 +1,55 @@
+﻿
+/* 5. Hacer un programa que solicite una serie de valores de tipo char (caracteres).
+      Se entiende por carácter a cada elemento que se obtiene de presionar una
+      tecla. Por ejemplo el valor “25” tiene dos caracteres (si quisiéramos guardarlo
+      en variables enteras nos alcanza con una, pero si queremos guardarlo en
+      variables char, necesitaremos dos); la frase “maxi programa” tiene 13 (se
+      incluye el espacio como un carácter).
+      La cantidad de valores será como máximo 50, pero el programa puede cortar
+      antes si se ingresa el carácter “.” (punto). Una vez cargado el vector de char,
+      recorrerlo y reemplazar todas las apariciones de la letra “a” por la letra “e”,
+      por ejemplo:
+      
+      Vector char original: “Hola muchachada cómo están”.
+      Vector char modificado: “Hole muchechede cómo esten”
+
+      Finalmente, mostrar el resultado en pantalla.
+      Nota: necesitaremos un vector char de 50, pero no lo cargaremos con un For. */
+
+using System;
+
+namespace guia8_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] cadena = new char[50];
+            
+            Console.Write("Ingrese un caracter: ");
+            char letra = char.Parse(Console.ReadLine());
+            
+            int i = 0;
+
+            while(letra != '.' && i < 50){
+                cadena[i] = letra;
+                Console.Write("Ingrese otro caracter: ");
+                letra = char.Parse(Console.ReadLine());
+                i++;
+            }
+            
+            for (int x = 0; x < i; x++)
+            {
+                if(cadena[x] == 'a'){
+                    cadena[x] = 'e';
+                }
+            }
+
+            Console.Write("\nPALABRA: ");
+            for (int x = 0; x < i; x++)
+            {
+                Console.Write(cadena[x]);
+            }
+        }
+    }
+}
---
+++ guia8_8/Program.cs
@@ -0,0 +1,52 @@
+﻿
+/* 8. Se ingresa una lista de 20 números en un vector. Se pide ordenar dichos
+      números en forma decreciente (de mayor a menor). Mostrar el listado
+      ordenado informando también la posición original de cada número en el
+      vector.
+      Pista: usar ciclos combinados. */
+
+using System;
+
+namespace guia8_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[20];
+            int[] posiciones = new int[20];
+
+            for (int x = 0; x < 20; x++)
+            {
+                Console.Write("Número: ");
+                int numero = int.Parse(Console.ReadLine());
+
+                numeros[x] = numero;
+                posiciones[x] = x + 1;
+            }
+
+            //MÉTODO BURBUJA:
+            for (int y = 0; y < 20; y++)
+            {
+                for (int x = 0; x < 19; x++)
+                {
+                    if(numeros[x] < numeros[x + 1]){
+                        int aux = numeros[x + 1];
+                        numeros[x + 1] = numeros[x];
+                        numeros[x] = aux;
+
+                        aux = posiciones[x + 1];
+                        posiciones[x + 1] = posiciones[x];
+                        posiciones[x] = aux;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nRESULTADOS:");
+            for (int x = 0; x < 20; x++)
+            {
+                Console.WriteLine("\nNúmero: " + numeros[x] + "\nPosición original: " + posiciones[x]);
+            }
+        }
+    }
+}
